1ff61a7e348e8df17bb5f51e230bbd6515f57869
kifish <huakaihuakai@qq.com>
2018-12-24 14:57:28 +0800

    completed

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-from src.model import BiLSTM_crf
-from src.utils import load_data_and_labels,save_pred,transformer_x,transformer_y
-from sklearn_crfsuite import metrics
+import os,utils
+import numpy as np
+from model import BiLSTM_crf
+from utils import load_data_and_labels,load_data,save_pred,transformer_x,transformer_y
 
+os.environ["CUDA_VISIBLE_DEVICES"]="-1" # just use cpu
+
+# load data and labels
 x_train, y_train = load_data_and_labels('../data/train.txt')
 transformer_x = transformer_x()
 x_train = transformer_x.fit(x_train)
@@ -9,37 +13,64 @@ vocab_size = transformer_x.vocab_size
 transformer_y = transformer_y(transformer_x.max_len)
 y_train = transformer_y.to_onehot(y_train)
 
-use_crf = True
-model = BiLSTM_crf(num_labels=8, word_vocab_size = vocab_size,max_seq_len=transformer_x.max_len,use_crf = use_crf)
+use_pretrain_embedding = True
+if use_pretrain_embedding:
+    # load word2vec
+    word2vec = {}
+    with open('../data/word2vec.txt', 'r', encoding='utf8') as f:
+        f.readline()  # 第一行是word数量以及embedding size;跳过
+        for line in f.readlines():
+            values = line.split()
+            word = values[0]
+            vec = np.asarray(values[1:], dtype='float32')
+            word2vec[word] = vec
+
+    print('Found %s word vectors.' % len(word2vec))
+
+    # init embedding_layer
+    EMBEDDING_DIM = 100
+    embedding_matrix = np.zeros((vocab_size, EMBEDDING_DIM))
+    word2id = transformer_x.word2id
+    for word, vec in word2vec.items():
+        if word in word2id:
+            embedding_matrix[word2id[word]] = word2vec[word]
+
+    model = BiLSTM_crf(num_labels = 8, embedding_matrix=embedding_matrix, max_seq_len=transformer_x.max_len,use_crf = True)
+
+else:
+    model = BiLSTM_crf(num_labels = 8,word_vocab_size = vocab_size, max_seq_len = transformer_x.max_len,use_crf = True)
+
 model = model.build()
 model.summary()
 
 
-# model.fit(x_train,y_train,verbose = 1,batch_size = 12,epochs= 1)
-x_test, y_test = load_data_and_labels('../data/dev.txt')
+x_val, y_val = load_data_and_labels('../data/dev.txt')
+x_val_len = list(map(len,x_val))
+x_val = transformer_x.tran(x_val)
+y_val_onehot = transformer_y.to_onehot(y_val)
+model.fit(x_train,y_train,validation_data = (x_val,y_val_onehot),verbose = 1,batch_size = 12,epochs= 15)
+
+y_pred = model.predict(x_val)
+y_pred = [seq[-x_val_len[idx]:] for idx, seq in enumerate(y_pred)] # safely de-padding
+y_pred = transformer_y.to_tag(y_pred)
+
+utils.eval(y_val,y_pred)
+
+x_test = load_data('../data/test.txt')
+x_test_len = list(map(len,x_test))
+
 x_test = transformer_x.tran(x_test)
-y_test_onehot = transformer_y.to_onehot(y_test)
-model.fit(x_train,y_train,validation_data = (x_test,y_test_onehot),verbose = 1,epochs= 1)
-
 pred = model.predict(x_test)
-
-if use_crf:
-    pass
-else:
-    # 直接选argmax？ 这种的肯定不如verterbi解码好。
-    pass
+pred = [seq[-x_test_len[idx]:] for idx, seq in enumerate(pred)]
 pred = transformer_y.to_tag(pred)
-print(metrics.flat_f1_score(y_test, pred,
-                      average='weighted', labels=transformer_y.tags))
 
-# group B and I results
-labels = transformer_y.tags
-sorted_labels = sorted(
-    labels,
-    key=lambda name: (name[1:], name[0])
-)
-print(metrics.flat_classification_report(
-    y_test, pred, labels=sorted_labels, digits=3
-))
-save_pred(pred)
+if use_pretrain_embedding:
+    if not os.path.exists('../data/pretrain_embedding'):
+        os.mkdir('../data/pretrain_embedding')
+    save_path = '../data/pretrain_embedding/pred.txt'
+else:
+    if not os.path.exists('../data/no_pretrain_embedding'):
+        os.mkdir('../data/no_pretrain_embedding')
+    save_path = '../data/no_pretrain_embedding/pred.txt'
 
+save_pred(pred,save_path,for_validation = False)
